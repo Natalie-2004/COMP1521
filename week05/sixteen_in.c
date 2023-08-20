b968b94d8941a2cfb34cae2aa1a8bf9f021be450
Natalie Ye <z5453932@ad.unsw.edu.au>
2023-08-20 22:26:35 +1000

    Update sixteen_in.c

--- week05/sixteen_in.c
+++ week05/sixteen_in.c
@@ -24,25 +24,19 @@ int main(int argc, char *argv[]) {
 //
 int16_t sixteen_in(char *bits) {
 
-    // PUT YOUR CODE HERE
-    // error check: 
-    // It checks if the length of the input string bits is exactly 16 characters. 
-    // If not, the program will terminate.
-    assert(strlen(bits) == N_BITS);
-
     int16_t result = 0;
 
     for (int i = 0; i < N_BITS; i++) {
         if (bits[i] == '1') {
-            // (N_BITS - i - 1)
+            // (15 - i)
             // calculates how many positions to the left the 1 should be shifted.
             // i.e for first iteration, with i = 0
             // the shift will be 15 positions to the left -> most significant figure
-            // 1 << (N_BITS - i - 1)
+            // 1 << (15 - i)
             // shifts the number 1 to the left by the calculated number of positions
             // result |== ...
             // ORing any number with 0 will leave it unchanged, and ORing it with 1 will set that bit
-            result |= 1 << (N_BITS - i - 1);
+            result |= 1 << (15 - i);
         }
     }
 
